e652bcc88d20d3567d0b7942fc49947763ffd424
Harsh Tuwar <harsh@touchline.com>
2020-03-31 18:44:15 -0300

    backend is Readygit add . :)

---
+++ config/db.js
@@ -0,0 +1,20 @@
+// file used to connect to our DB :)
+
+const mongoose = require('mongoose');
+
+const connectDB = async () => {
+    try {
+        const conn = await mongoose.connect(process.env.MONGO_URI, {
+            useNewUrlParser: true,
+            useCreateIndex: true,
+            useUnifiedTopology: true
+        });
+
+        console.log(`Mongo DB connected: ${conn.connection.host}`.cyan.underline.bold);
+    } catch (err) {
+        console.log(`Error: ${err.message}`.red);
+        process.exit(1);
+    }
+}
+
+module.exports = connectDB;
